b15eefeb620cc80b024990943c9a9dd5186b4c94
Gangos <sevensense357@gmail.com>
2020-03-05 18:54:13 +0700

    simple calc

--- calc/main.js
+++ calc/main.js
@@ -1,31 +1,26 @@
 (function () {
 
-  const operators = document.querySelectorAll('.calc__btn');
-
   const input1 = document.getElementById('input1');
   const input2 = document.getElementById('input2');
 
-  operators.forEach(function (element) {
-    element.addEventListener('click', function (e) {
-      let Number1 = Number(input1.value);
-      let Number2 = Number(input2.value);
-      let result = document.createElement('div');
-      result.className = 'result';
-      if (e.currentTarget.innerText === '+') {
-        result.textContent = Number1 + Number2
-      } else if (e.currentTarget.innerText === '-') {
-        result.textContent = Number1 - Number2
-      } else if (e.currentTarget.innerText === '*') {
-        result.textContent = Number1 * Number2
-      } else if (e.currentTarget.innerText === '/') {
-        result.textContent = Number1 / Number2
-      }
-      document.body.append(result);
-      setTimeout(function () {
-        result.remove()
-      }, 2000);
-    });
-  })
+  document.addEventListener('click', function (e) {
+    let Number1 = Number(input1.value);
+    let Number2 = Number(input2.value);
+    let result = document.createElement('div');
+    result.className = 'result';
+    if (e.target.dataset.operator === '+') {
+      result.textContent = Number1 + Number2
+    } else if (e.target.dataset.operator === '-') {
+      result.textContent = Number1 - Number2
+    } else if (e.target.dataset.operator === '*') {
+      result.textContent = Number1 * Number2
+    } else if (e.target.dataset.operator === '/') {
+      result.textContent = Number1 / Number2
+    }
+    document.body.append(result);
+    setTimeout(function () {
+      result.remove()
+    }, 2000);
+  });
 
 })();
-
