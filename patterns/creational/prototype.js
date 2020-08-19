2f65789cb84c0eff73b6c9ca3a56c64b82315f1c
Ruslan <ruslan@pyrobyte.ru>
2020-08-19 08:58:02 +0700

    patterns

--- patterns/creational/prototype.js
+++ patterns/creational/prototype.js
@@ -1,19 +1,19 @@
 class LamborghiniCar {
-  constructor(model, price, interior, autopilot) {
-    this.model = model;
-    this.price = price;
-    this.interior = interior;
-    this.autopilot = autopilot;
-  }
+    constructor(model, price, interior, autopilot) {
+        this.model = model;
+        this.price = price;
+        this.interior = interior;
+        this.autopilot = autopilot;
+    }
 
-  produce() {
-    return new LamborghiniCar(
-      this.model,
-      this.price,
-      this.interior,
-      this.autopilot
-    );
-  }
+    produce() {
+        return new LamborghiniCar(
+            this.model,
+            this.price,
+            this.interior,
+            this.autopilot
+        );
+    }
 }
 
 const prototypeCar = new LamborghiniCar("Aventador", 20000, "white", true);
@@ -25,6 +25,6 @@ const car3 = prototypeCar.produce();
 car1.interior = "black";
 car1.autopilot = false;
 
-console.log(car1)
-console.log(car2)
-console.log(car3)
+console.log(car1);
+console.log(car2);
+console.log(car3);
